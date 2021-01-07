a48b747ff63470dc1c9c0299c741edc3743d16fe
Alexandr <mail@nomail.ru>
2021-01-08 00:47:58 +0300

    upload

---
+++ Буфер обмена/Form1.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 142)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(431, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Включить"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Замена", "Добавление", "Регистр", "Замена + Добавление", "Добавление + Регистр", "Замена + Регистр", "Замена + Добавление + Регистр"})
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 28)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(175, 21)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(175, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Выберите тип изменений"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Enabled = False
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label2.Location = New System.Drawing.Point(9, 63)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(280, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Введите символ и режим для добавление"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(12, 106)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(175, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox2.Enabled = False
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"Добавление слева", "Добавление справа", "Добавление слева и справа"})
+        Me.ComboBox2.Location = New System.Drawing.Point(12, 82)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(175, 21)
+        Me.ComboBox2.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Enabled = False
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label3.Location = New System.Drawing.Point(337, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(105, 16)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Что заменить?"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Location = New System.Drawing.Point(290, 28)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(152, 20)
+        Me.TextBox2.TabIndex = 7
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Enabled = False
+        Me.TextBox3.Location = New System.Drawing.Point(290, 73)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(152, 20)
+        Me.TextBox3.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Enabled = False
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label4.Location = New System.Drawing.Point(317, 51)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(125, 16)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "На что заменить?"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Enabled = False
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label5.Location = New System.Drawing.Point(352, 96)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(61, 16)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Регистр"
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox3.Enabled = False
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Items.AddRange(New Object() {"Низкий", "Высокий"})
+        Me.ComboBox3.Location = New System.Drawing.Point(267, 114)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(175, 21)
+        Me.ComboBox3.TabIndex = 11
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(455, 177)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Буфер обмена"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox3 As System.Windows.Forms.ComboBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ Буфер обмена/Form1.vb
@@ -0,0 +1,291 @@
+﻿Public Class Form1
+    Dim on_off As Boolean = False
+    'All mode
+    Dim mode As Integer = -1
+    'Add
+    Dim add_mode As Integer = -1, add_text As String = ""
+    'Add repeat
+    Dim add_repeat As String = ""
+    'Replace
+    Dim replace_a As String = "", replace_b As String = ""
+    'Registr
+    Dim registr As Integer = -1
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If on_off = False Then
+            on_off = True
+            Button1.Text = "Выключить"
+            Timer1.Enabled = True
+        Else
+            on_off = False
+            Button1.Text = "Включить"
+            Timer1.Enabled = False
+        End If
+    End Sub
+    Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Label2.Enabled = False
+        Label3.Enabled = False
+        Label4.Enabled = False
+        Label5.Enabled = False
+        TextBox1.Enabled = False
+        TextBox2.Enabled = False
+        TextBox3.Enabled = False
+        ComboBox2.Enabled = False
+        ComboBox3.Enabled = False
+        If ComboBox1.SelectedIndex = 0 Then
+            Label3.Enabled = True
+            Label4.Enabled = True
+            TextBox2.Enabled = True
+            TextBox3.Enabled = True
+            mode = 0
+        ElseIf ComboBox1.SelectedIndex = 1 Then
+            Label2.Enabled = True
+            ComboBox2.Enabled = True
+            TextBox1.Enabled = True
+            mode = 1
+        ElseIf ComboBox1.SelectedIndex = 2 Then
+            Label5.Enabled = True
+            ComboBox3.Enabled = True
+            mode = 2
+        ElseIf ComboBox1.SelectedIndex = 3 Then
+            Label3.Enabled = True
+            Label4.Enabled = True
+            TextBox2.Enabled = True
+            TextBox3.Enabled = True
+            Label2.Enabled = True
+            ComboBox2.Enabled = True
+            TextBox1.Enabled = True
+            mode = 3
+        ElseIf ComboBox1.SelectedIndex = 4 Then
+            Label2.Enabled = True
+            ComboBox2.Enabled = True
+            TextBox1.Enabled = True
+            Label5.Enabled = True
+            ComboBox3.Enabled = True
+            mode = 4
+        ElseIf ComboBox1.SelectedIndex = 5 Then
+            Label5.Enabled = True
+            ComboBox3.Enabled = True
+            Label3.Enabled = True
+            Label4.Enabled = True
+            TextBox2.Enabled = True
+            TextBox3.Enabled = True
+            mode = 5
+        ElseIf ComboBox1.SelectedIndex = 6 Then
+            Label3.Enabled = True
+            Label4.Enabled = True
+            TextBox2.Enabled = True
+            TextBox3.Enabled = True
+            Label2.Enabled = True
+            ComboBox2.Enabled = True
+            TextBox1.Enabled = True
+            Label5.Enabled = True
+            ComboBox3.Enabled = True
+            mode = 6
+        End If
+    End Sub
+    Private Sub ComboBox3_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox3.SelectedIndexChanged
+        If ComboBox3.SelectedIndex = 0 Then
+            registr = 0
+        ElseIf ComboBox3.SelectedIndex = 1 Then
+            registr = 1
+        End If
+    End Sub
+    Private Sub ComboBox2_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox2.SelectedIndexChanged
+        If ComboBox2.SelectedIndex = 0 Then
+            add_mode = 0
+        ElseIf ComboBox2.SelectedIndex = 1 Then
+            add_mode = 1
+        ElseIf ComboBox2.SelectedIndex = 2 Then
+            add_mode = 2
+        End If
+        add_repeat = ""
+    End Sub
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+        add_text = TextBox1.Text
+    End Sub
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.TextChanged
+        replace_a = TextBox2.Text
+    End Sub
+    Private Sub TextBox3_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox3.TextChanged
+        replace_b = TextBox3.Text
+    End Sub
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Dim times As String
+        If mode = 0 Then
+            If My.Computer.Clipboard.ContainsText() Then
+                times = My.Computer.Clipboard.GetText()
+                times = Replace(times, replace_a, replace_b)
+                My.Computer.Clipboard.SetText(times)
+            End If
+        ElseIf mode = 1 Then
+            If My.Computer.Clipboard.GetText() <> add_repeat Then
+                If add_mode = 0 Then
+                    If My.Computer.Clipboard.ContainsText() Then
+                        times = My.Computer.Clipboard.GetText()
+                        times = add_text & times
+                        add_repeat = times
+                        My.Computer.Clipboard.SetText(times)
+                    End If
+                ElseIf add_mode = 1 Then
+                    If My.Computer.Clipboard.ContainsText() Then
+                        times = My.Computer.Clipboard.GetText()
+                        times = times & add_text
+                        add_repeat = times
+                        My.Computer.Clipboard.SetText(times)
+                    End If
+                ElseIf add_mode = 2 Then
+                    If My.Computer.Clipboard.ContainsText() Then
+                        times = My.Computer.Clipboard.GetText()
+                        times = add_text & times & add_text
+                        add_repeat = times
+                        My.Computer.Clipboard.SetText(times)
+                    End If
+                End If
+            End If
+        ElseIf mode = 2 Then
+            If registr = 0 Then
+                If My.Computer.Clipboard.ContainsText() Then
+                    times = My.Computer.Clipboard.GetText()
+                    times = (times).ToLower
+                    My.Computer.Clipboard.SetText(times)
+                End If
+            ElseIf registr = 1 Then
+                If My.Computer.Clipboard.ContainsText() Then
+                    times = My.Computer.Clipboard.GetText()
+                    times = (times).ToUpper
+                    My.Computer.Clipboard.SetText(times)
+                End If
+            End If
+        ElseIf mode = 3 Then
+            If My.Computer.Clipboard.ContainsText() Then
+                times = My.Computer.Clipboard.GetText()
+                times = Replace(times, replace_a, replace_b)
+                My.Computer.Clipboard.SetText(times)
+            End If
+            If My.Computer.Clipboard.GetText() <> add_repeat Then
+                If add_mode = 0 Then
+                    If My.Computer.Clipboard.ContainsText() Then
+                        times = My.Computer.Clipboard.GetText()
+                        times = add_text & times
+                        add_repeat = times
+                        My.Computer.Clipboard.SetText(times)
+                    End If
+                ElseIf add_mode = 1 Then
+                    If My.Computer.Clipboard.ContainsText() Then
+                        times = My.Computer.Clipboard.GetText()
+                        times = times & add_text
+                        add_repeat = times
+                        My.Computer.Clipboard.SetText(times)
+                    End If
+                ElseIf add_mode = 2 Then
+                    If My.Computer.Clipboard.ContainsText() Then
+                        times = My.Computer.Clipboard.GetText()
+                        times = add_text & times & add_text
+                        add_repeat = times
+                        My.Computer.Clipboard.SetText(times)
+                    End If
+                End If
+            End If
+        ElseIf mode = 4 Then
+            If My.Computer.Clipboard.GetText() <> add_repeat Then
+                If add_mode = 0 Then
+                    If My.Computer.Clipboard.ContainsText() Then
+                        times = My.Computer.Clipboard.GetText()
+                        times = add_text & times
+                        add_repeat = times
+                        My.Computer.Clipboard.SetText(times)
+                    End If
+                ElseIf add_mode = 1 Then
+                    If My.Computer.Clipboard.ContainsText() Then
+                        times = My.Computer.Clipboard.GetText()
+                        times = times & add_text
+                        add_repeat = times
+                        My.Computer.Clipboard.SetText(times)
+                    End If
+                ElseIf add_mode = 2 Then
+                    If My.Computer.Clipboard.ContainsText() Then
+                        times = My.Computer.Clipboard.GetText()
+                        times = add_text & times & add_text
+                        add_repeat = times
+                        My.Computer.Clipboard.SetText(times)
+                    End If
+                End If
+            End If
+            If registr = 0 Then
+                If My.Computer.Clipboard.ContainsText() Then
+                    times = My.Computer.Clipboard.GetText()
+                    times = (times).ToLower
+                    My.Computer.Clipboard.SetText(times)
+                End If
+            ElseIf registr = 1 Then
+                If My.Computer.Clipboard.ContainsText() Then
+                    times = My.Computer.Clipboard.GetText()
+                    times = (times).ToUpper
+                    My.Computer.Clipboard.SetText(times)
+                End If
+            End If
+        ElseIf mode = 5 Then
+            If My.Computer.Clipboard.ContainsText() Then
+                times = My.Computer.Clipboard.GetText()
+                times = Replace(times, replace_a, replace_b)
+                My.Computer.Clipboard.SetText(times)
+            End If
+            If registr = 0 Then
+                If My.Computer.Clipboard.ContainsText() Then
+                    times = My.Computer.Clipboard.GetText()
+                    times = (times).ToLower
+                    My.Computer.Clipboard.SetText(times)
+                End If
+            ElseIf registr = 1 Then
+                If My.Computer.Clipboard.ContainsText() Then
+                    times = My.Computer.Clipboard.GetText()
+                    times = (times).ToUpper
+                    My.Computer.Clipboard.SetText(times)
+                End If
+            End If
+        ElseIf mode = 6 Then
+            If My.Computer.Clipboard.ContainsText() Then
+                times = My.Computer.Clipboard.GetText()
+                times = Replace(times, replace_a, replace_b)
+                My.Computer.Clipboard.SetText(times)
+            End If
+            If My.Computer.Clipboard.GetText() <> add_repeat Then
+                If add_mode = 0 Then
+                    If My.Computer.Clipboard.ContainsText() Then
+                        times = My.Computer.Clipboard.GetText()
+                        times = add_text & times
+                        add_repeat = times
+                        My.Computer.Clipboard.SetText(times)
+                    End If
+                ElseIf add_mode = 1 Then
+                    If My.Computer.Clipboard.ContainsText() Then
+                        times = My.Computer.Clipboard.GetText()
+                        times = times & add_text
+                        add_repeat = times
+                        My.Computer.Clipboard.SetText(times)
+                    End If
+                ElseIf add_mode = 2 Then
+                    If My.Computer.Clipboard.ContainsText() Then
+                        times = My.Computer.Clipboard.GetText()
+                        times = add_text & times & add_text
+                        add_repeat = times
+                        My.Computer.Clipboard.SetText(times)
+                    End If
+                End If
+            End If
+            If registr = 0 Then
+                If My.Computer.Clipboard.ContainsText() Then
+                    times = My.Computer.Clipboard.GetText()
+                    times = (times).ToLower
+                    My.Computer.Clipboard.SetText(times)
+                End If
+            ElseIf registr = 1 Then
+                If My.Computer.Clipboard.ContainsText() Then
+                    times = My.Computer.Clipboard.GetText()
+                    times = (times).ToUpper
+                    My.Computer.Clipboard.SetText(times)
+                End If
+            End If
+        End If
+    End Sub
+End Class
